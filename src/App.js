addba83ab6106d85c47ae42e85d9ab78701322b7
Lenny Sentuc <lenny.sentuc@hotmail.fr>
2023-11-10 14:57:30 +0100

    Link au backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [quizList, setQuizList] = useState([]);
+
+  useEffect(() => {
+
+    axios.get('http://localhost:3000/api/quiz')
+      .then(response => {setQuizList(response.data)})
+      .catch(error => {console.error("Impossible de récupérer les Questions :", error)});
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Liste des Questions</h1>
+      <ul>
+        {quizList.map(quiz => (
+          <li key={quiz._id}>
+            <div>
+              <h3>{quiz.question}</h3>
+              <ul>
+                {quiz.options.map((answer, index) => (
+                  <li key={index}>{answer}</li>
+                ))}
+              </ul>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
