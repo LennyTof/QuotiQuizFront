0b035cd36c9b913c13f444ebe945692d3b17ecf9
Lenny Sentuc <lenny.sentuc@hotmail.fr>
2023-11-13 23:30:33 +0100

    affichage resultat

--- src/Components/ResultPage.js
+++ src/Components/ResultPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import '../style/result.css';
 
 const ResultPage = () => {
   const location = useLocation();
@@ -10,11 +11,13 @@ const ResultPage = () => {
 
   return (
     <div>
-      <h1>C'est terminé pour aujourd'hui !</h1>
-      <h2>
-        Tu as correctement répondu à {numberOfCorrectAnswer} {questionPlural} sur 5 !
-      </h2>
-      <h4>Rendez-vous demain pour un autre quiz !</h4>
+      <h1 className="mt-3">C'est terminé pour aujourd'hui !</h1>
+      <div className="container">
+        <h2 className="result">
+          Tu as correctement répondu à {numberOfCorrectAnswer} {questionPlural} sur 5 !
+        </h2>
+    </div>
+    <h4>Rendez-vous demain pour un autre quiz !</h4>
     </div>
   );
 };
