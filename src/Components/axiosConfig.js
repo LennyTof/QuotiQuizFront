759e2fd7e1806f940e01115c126ba0b91b354ace
Lenny Sentuc <lenny.sentuc@hotmail.fr>
2024-05-10 16:43:43 +0200

    Ajout d'un .env et changement au niveau des variables

--- src/Components/axiosConfig.js
+++ src/Components/axiosConfig.js
@@ -1,10 +1,17 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3000/api';
+const apiURL = process.env.REACT_APP_API_URL;
+axios.defaults.baseURL = apiURL;
+
 axios.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
   config.headers.Authorization = token ? `Bearer ${token}` : '';
   return config;
+});
+
+axios.interceptors.response.use(response => response, error => {
+  console.error('An API error occurred', error);
+  return Promise.reject(error);
 });
 
 export default axios;
